f282188486f92796c283ab10adab99a1659aeb71
Ruben Vitt <r.vitt@fme.de>
2019-12-07 15:21:09 +0100

    feat(frontend): move files into frontend

--- src/services/theming/DefaultClasses.ts
+++
@@ -1,9 +0,0 @@
-import {makeStyles} from "@material-ui/core";
-
-export function defaultClasses() {
-    return makeStyles({
-       root: {
-           backgroundColor: 'green'
-       }
-    });
-}
