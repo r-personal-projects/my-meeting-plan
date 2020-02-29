837c3e36cd2db1bbce711f5a4f9d86a48e127eb8
Ruben Vitt <ruben.vitt@outlook.de>
2020-02-29 12:20:57 +0100

    feat(frontend): some changes

--- frontend/src/components/home/Home.component.tsx
+++ frontend/src/components/home/Home.component.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {Segment} from "semantic-ui-react";
-import style from './home.style.scss';
+import style from './home.style.less';
 
 function HomeComponent() {
     return (
         <Segment className={style.header}>
 
         </Segment>
-    );
+    )
 }
 
 
